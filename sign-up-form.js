2e7c656e6bd5a2bba10bd084f88b441aa0089ebd
sabirama <markcondes21@gmail.com>
2023-09-05 22:46:27 +0800

    edited js

--- sign-up-form.js
+++ sign-up-form.js
@@ -6,7 +6,6 @@ form.addEventListener("submit", function (event) {
   const username = document.getElementById("username");
   const password = document.getElementById("password");
   const confirmPass = document.getElementById("confirm-password");
-  console.log(username.value + password.value + confirmPass.value);
 
   if (username.value && password.value && confirmPass.value) {
     if (
@@ -14,12 +13,39 @@ form.addEventListener("submit", function (event) {
       password.value.length > 7 &&
       password.value === confirmPass.value
     ) {
-      for (let i = 0; i < username.value.length; i++)
+      capitalLetter = false;
+      for (let i = 0; i < username.value.length; i++) {
         if (
-          username.value.charAt(i) === username.value.charAt(i).toUpperCase()
+          username.value.charAt(i) !== username.value.charAt(i).toLowerCase()
         ) {
-          console.log("Sign-up successful");
+          capitalLetter = true;
         }
-    } else alert("Error: please check your input if it matches the criteria.");
-  } else alert("Error: please input username and password.");
+      }
+      if (capitalLetter == true) {
+        alert("Signed-up Succesfully.");
+      } else alert("Please check your username.");
+    } else {
+      alert("please check your input if it matches the criteria.");
+    }
+  } else {
+    alert("please put in username and password.");
+  }
+});
+
+const newsletter = document.getElementById("newsletter");
+
+newsletter.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log(newsletter);
+
+  const email = document.getElementById("newsletter-email");
+  if (
+    email.value.includes("@") &&
+    email.value.includes(".") &&
+    email.value.length > 7
+  ) {
+    alert("You are now subscribed to newsletter.");
+  } else {
+    alert("Please put in a valid email.");
+  }
 });
