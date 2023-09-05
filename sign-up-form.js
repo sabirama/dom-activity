470aae2792bcf0a965436dead43e11356aa80afc
sabirama <markcondes21@gmail.com>
2023-09-05 15:34:16 +0800

    form with validation

--- sign-up-form.js
+++ sign-up-form.js
@@ -1,13 +1,25 @@
-const singUpBtn = document.getElementById("sign-up-btn");
-singUpBtn.addEventListener("click", validateForm());
+const form = document.getElementById("sign-up-form");
 
-function validateForm() {
-  const form = document.getElementById("sign-up-form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
   const username = document.getElementById("username");
   const password = document.getElementById("password");
   const confirmPass = document.getElementById("confirm-password");
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-  });
   console.log(username.value + password.value + confirmPass.value);
-}
+
+  if (username.value && password.value && confirmPass.value) {
+    if (
+      username.value.length > 7 &&
+      password.value.length > 7 &&
+      password.value === confirmPass.value
+    ) {
+      for (let i = 0; i < username.value.length; i++)
+        if (
+          username.value.charAt(i) === username.value.charAt(i).toUpperCase()
+        ) {
+          console.log("Sign-up successful");
+        }
+    } else alert("Error: please check your input if it matches the criteria.");
+  } else alert("Error: please input username and password.");
+});
